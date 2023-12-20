cf19f19039ec08969183b91f0f72465f09d0c87a
sole1982 <soleadhernandez@gmail.com>
2023-12-20 20:37:46 -0300

    list usuario y delete

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import LoginUsuario
+from .views import *
 from django.contrib.auth import views as auth_views
 
 app_name = 'apps.usuario'
@@ -13,4 +13,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
+    path('usuario/ <int:pk>/eliminar/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 ]
