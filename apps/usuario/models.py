b5236cab70e7ceb00137144c62f11aaa47e8909e
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-21 02:24:36 -0300

    Modifico js para controlar menú desplegable usuarios

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Usuario(AbstractUser):
-    imagen = models.ImageField(null=True, blank=True, upload_to='usuario', default='.static/img/usu_default_navidad.jpg')
+    imagen = models.ImageField(null=True, blank=True, upload_to='usuario', default='usuario/user-default.png')
     
     def get_absolute_url(self):
         return reverse('index')
-    
