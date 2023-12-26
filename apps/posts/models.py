e90fcf420821fd05114177b2bbf6a6e806196e31
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-12-26 19:31:20 -0300

    commit para reverse

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -33,8 +33,8 @@ class Post(models.Model):
             super().delete()
 
 class Comentario(models.Model):
-      posts: models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='comentarios')
-      usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='comentarios' )
+      post: models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='comentarios')
+      usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comentarios' )
       texto = models.TextField()
       fecha = models.DateTimeField( auto_now_add= True)
 
