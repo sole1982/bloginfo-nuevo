b0fafd18c034b69cdafe6b4dfb570d80c327e13d
sole1982 <soleadhernandez@gmail.com>
2023-12-27 20:48:52 -0300

    post y categorias

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
-    path('usuario/ <int:pk>/eliminar/', UsuarioDeleteView.as_view(), name='usuario_delete'),
-    path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
+   
+
+    path('usuario/', UsuarioListView.as_view(), name='usuario_list'),
     path('usuario/<int:pk>/eliminar/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 ]
